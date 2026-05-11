c0b712033c6f7b5b417feee29458d6caf0de7f03
Amon Eliel <amonpaterne7@gmail.com>
2026-05-11 21:28:34 +0000

    Code modifié

--- src/NombreDeMots.java
+++ src/NombreDeMots.java
@@ -1,9 +1,10 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class NombreDeMots {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         // Nom du fichier à lire
         String nomFichier = "fichier.txt";
@@ -11,42 +12,37 @@ public class NombreDeMots {
         // Compteur total de mots
         int totalMots = 0;
 
-        try {
-            // Ouvrir le fichier avec le lecteur de fichiers
-            File fichier = new File(nomFichier);
-            Scanner lecteur = new Scanner(fichier);
+        // Ouvrir le fichier avec le lecteur de fichiers
+        File fichier = new File(nomFichier);
+        Scanner lecteur = new Scanner(fichier);
 
-            System.out.println("Comptage de mots : " + nomFichier + "\n");
+        System.out.println("Comptage de mots : " + nomFichier + "\n");
 
-            // Boucle qui lit chaque ligne
-            while (lecteur.hasNextLine()) {
+        // Boucle qui lit chaque ligne
+        while (lecteur.hasNextLine()) {
 
-                // Lire la ligne courante
-                String ligne = lecteur.nextLine();
+            // Lire la ligne courante
+            String ligne = lecteur.nextLine();
 
-                // Séparer la ligne par les espaces
-                String[] mots = ligne.split(" ");
+            // Séparer la ligne par les espaces
+            String[] mots = ligne.split(" ");
 
-                // Compter les mots de cette ligne
-                int nombreMotsLigne = mots.length;
+            // Compter les mots de cette ligne
+            int nombreMotsLigne = mots.length;
 
-                // Afficher la ligne et son nombre de mots
-                System.out.println("Ligne : \"" + ligne + "\"");
-                System.out.println("Mots dans cette ligne : " + nombreMotsLigne);
-                System.out.println("\n");
+            // Afficher la ligne et son nombre de mots
+            System.out.println("Ligne : \"" + ligne + "\"");
+            System.out.println("Mots dans cette ligne : " + nombreMotsLigne);
+            System.out.println("\n");
 
-                // Additionner au total
-                totalMots += nombreMotsLigne;
-            }
-
-            // Fermer le lecteur
-            lecteur.close();
-
-            // Afficher le total
-            System.out.println("\n TOTAL : " + totalMots + " mots ");
-
-        } catch (Exception e) {
-            System.out.println("Fichier non trouvé : " + nomFichier);
+            // Additionner au total
+            totalMots += nombreMotsLigne;
         }
+
+        // Fermer le lecteur
+        lecteur.close();
+
+        // Afficher le total
+        System.out.println("\n TOTAL : " + totalMots + " mots ");
     }
 }
